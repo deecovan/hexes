7f430ef70ed72480fae12b3b80b2cb73b44bff12
Dee <dee.covan@gmail.com>
2025-07-17 14:41:12 +0300

    submodule add UseThis/DragAndDropDemos

--- UseThis/Godot-demos-new-signals-syntax/Godot4-New-Signal-Syntax/annotations/player.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-@export_node_path(TextEdit, GPUParticlesCollisionHeightField3D)
-
-
--- UseThis/Godot-demos-new-signals-syntax/Godot4-New-Signal-Syntax/annotations/ui_display.gd
+++
@@ -1,6 +0,0 @@
-extends Container
-
-@onready @export_node_path(LineEdit, TextEdit) var input_element
-
-func _ready():
-	add_child(input_element)
