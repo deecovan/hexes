97be62dc716b88f6d6e626c3b50ff48211981bc6
Dee <dee.covan@gmail.com>
2025-07-18 16:47:53 +0300

    Add principal Drag-and-Drop control

--- Godot_v4.4.1_project/Scripts/TablePart.gd
+++ Godot_v4.4.1_project/Scripts/TablePart.gd
@@ -1,22 +1,28 @@
 extends Control
 
 func make_data():
-	return null
-
+	printt("make_data()")
+	var new_card = $Card.duplicate()
+	return new_card
+	
 func make_preview(mydata):
-	return null
+	printt("make_preview(mydata)", mydata)
+	return mydata.duplicate()
 
-func _get_drag_data(position):
+func _get_drag_data(_data_position):
+	printt("_get_drag_data(_data_position)", _data_position)
 	var mydata = make_data() 
 	# This is your custom method generating the drag data.
 	set_drag_preview(make_preview(mydata)) 
 	# This is your custom method generating the preview of the drag data.
 	return mydata
 
-func _can_drop_data(position, data):
+func _can_drop_data(_data_position, data):
+	printt("_can_drop_data(position, data)", _data_position, data)
 	# Check position if it is relevant to you
 	# Otherwise, just check data
-	return typeof(data) == TYPE_DICTIONARY and data.has("expected")
+	return data is Button
 
-func _drop_data(position, data):
-	var color = data["color"]
+func _drop_data(_data_position, data):
+	printt("_drop_data(position, data)", _data_position, data)
+	# Implement Drop here
