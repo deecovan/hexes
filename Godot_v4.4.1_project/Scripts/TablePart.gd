4f72392050f256f58654e35d4dc5fd5e62ba3368
Dee <dee.covan@gmail.com>
2025-07-18 18:33:17 +0300

    Optimize Drag Implement Drop

--- Godot_v4.4.1_project/Scripts/TablePart.gd
+++ Godot_v4.4.1_project/Scripts/TablePart.gd
@@ -1,7 +1,13 @@
-extends VBoxContainer
+extends HBoxContainer
 
 func _can_drop_data(_data_position, data):
 	printt("_can_drop_data(position, data)", self, _data_position, data)
 	# Check position if it is relevant to you
 	# Otherwise, just check data
 	return data is Button
+
+func _drop_data(_data_position, data):
+	printt("_drop_data(position, data)", _data_position, data)
+	# Implement Drop here
+	add_child(data.duplicate(), true)
+	data.queue_free()
