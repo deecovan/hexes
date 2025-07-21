6145a6a230de39def957b2cabc5f004aa9d64fcd
Dee <dee.covan@gmail.com>
2025-07-21 22:17:28 +0300

    Docs MD files

---
+++ Docs/Index.cmd
@@ -0,0 +1,12 @@
+echo #[Abilities](Abilities.MD) > Abilities.MD
+echo #[Attach](Attach.MD) > Attach.MD
+echo #[Begin Turn](BeginTurn.MD) > BeginTurn.MD
+echo #[Coins](Coins.MD) > Coins.MD
+echo #[Draw](Draw.MD) > Draw.MD
+echo #[End Turn](EndTurn.MD) > EndTurn.MD
+echo #[G/W](GW.MD) > GW.MD
+echo #[G](G.MD) > G.MD
+echo #[Hexes](Hexes.MD) > Hexes.MD
+echo #[Play](Play.MD) > Play.MD
+echo #[Turn Sequence](TurnSequence.MD) > TurnSequence.MD
+echo #[W](W.MD) > W.MD
